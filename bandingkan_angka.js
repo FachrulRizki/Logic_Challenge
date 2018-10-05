dad45cf57256b92acb5966e8364e4f9ab7d25f73
FachrulRizki <42919255+FachrulRizki@users.noreply.github.com>
2018-10-05 10:15:40 +0700

    Update bandingkan_angka.js

--- bandingkan_angka.js
+++ bandingkan_angka.js
@@ -1,12 +1,11 @@
-var angka1 = 3
-var angka2 = 6
-
-function perbandingan(angka1, angka2) {
-	if(angka1 == angka2){
+function perbandingan(angka1,angka2) {
+	if (angka2 > angka1){
+		return true
+	}if (angka1 > angka2){
+		return false
+	}if (angka2 == angka1){
 		return -1
 	}
-	return angka1 < angka2
-}
-console.log(perbandingan(angka1, angka2))
-console.log(perbandingan(angka2, angka1))
-console.log(perbandingan(angka2, angka2))
+}	console.log(perbandingan(2,6))
+	console.log(perbandingan(6,2))
+	console.log(perbandingan(6,6))
